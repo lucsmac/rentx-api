8559b0d624bf307dfc4c0642dce63f588dc2b57e
Lucas Macedo <lucasmdc57@gmail.com>
2021-05-24 21:53:44 -0300

    creating create specification use case

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,8 +1,8 @@
-import { Specification } from "../model/Specifications";
+import { Specification } from "../../model/Specifications";
 import {
   ICreateSpecificationsDTO,
   ISpecificationsRepository,
-} from "./ISpecificationsRepository";
+} from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
