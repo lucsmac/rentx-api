d937f94529d6b1e059c15aca3a0360310ce8ba37
Lucas Macedo <lucasmdc57@gmail.com>
2021-05-28 20:58:58 -0300

    using tsyringe to create singleton

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import multer from "multer";
 
-import createCategoryController from "../modules/cars/useCases/createCategory";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { importCategoryController } from "../modules/cars/useCases/ImportCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+const createCategoryController = new CreateCategoryController();
 
 const upload = multer({
   dest: "./tmp",
@@ -15,9 +16,7 @@ categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
 });
 
-categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController().handle(request, response);
-});
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
   return importCategoryController.handle(request, response);
