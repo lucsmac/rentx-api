27e6bbb77a461096cd56dddb360e5fb9abe3b82f
Lucas Macedo <lucasmdc57@gmail.com>
2021-05-29 17:15:04 -0300

    implements avatar update route

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -28,6 +28,10 @@ export async function ensureAuthenticated(
     const user = usersRepository.findById(user_id);
     if (!user) throw new AppError("User doesn't not exist", 401);
 
+    request.user = {
+      id: user_id,
+    };
+
     next();
   } catch (err) {
     throw new AppError("Invalid token", 401);
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserControler";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 const createUserControle = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 usersRoutes.post("/", createUserControle.handle);
+
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
