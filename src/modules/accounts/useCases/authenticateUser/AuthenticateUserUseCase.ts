50e668416a60dce7c199c130f444c0789ea5ca71
Lucas Macedo <lucasmdc57@gmail.com>
2021-05-29 15:24:48 -0300

    add create session route

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,50 @@
+import { compare } from "bcrypt";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) throw new Error("Email or password incorrect!");
+
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) throw new Error("Email or password incorrect!");
+
+    const token = sign({}, "3af9acebff4cd77891a0baf0aa712db4", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
