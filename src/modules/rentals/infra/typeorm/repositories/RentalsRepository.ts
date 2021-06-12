063c4d9b85aa8d686749fb7bd42d971bddfd9d46
Lucas Macedo <lucasmdc57@gmail.com>
2021-06-12 16:20:21 -0300

    allow list all rentals by user id

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -53,6 +53,11 @@ class RentalsRepository implements IRentalsRepository {
 
     return rental;
   }
+
+  async findByUser(user_id: string): Promise<Rental[]> {
+    const rentals = await this.repository.find({ user_id });
+    return rentals;
+  }
 }
 
 export { RentalsRepository };
